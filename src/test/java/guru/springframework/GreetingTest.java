e3032777f0f1f76f7b78466ab405800616ff10df
Eduard Streltsov <streltsov.eduard@gmail.com>
2018-12-04 06:55:45 +0200

    S04L039

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,18 +1,39 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 	
+	private Greeting greeting;
+	
+	@BeforeAll
+	private static void beforeAll() {
+		System.out.println("before all");
+	}
+	
+	@AfterAll
+	private static void afterAll() {
+		System.out.println("after all");
+	}
+	
+	@BeforeEach
+	void setUp() {
+		System.out.println("before each");
+		greeting = new Greeting();
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("after each");
+	}
+	
 	@Test
 	void helloWorld() {
-		Greeting greeting = new Greeting();
 		System.out.println(greeting.helloWorld());
 	}
 	
 	@Test
 	void helloWorld1() {
-		Greeting greeting = new Greeting();
 		System.out.println(greeting.helloWorld("Username"));
 	}
 }
